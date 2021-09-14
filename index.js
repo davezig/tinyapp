98b252994f0ddcd211afffa66e22dbcebf5d0139
davezig <bam099@gmail.com>
2021-09-14 18:17:25 -0400

    created JS for moving left and right

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let apps = ['app', 'blog', 'shop', 'note', 'game', 'grave'];
 
 let currentApp = document.getElementById('app-name');
-currentApp.innerHTML = apps[2];
+currentApp.innerHTML = apps[0];
+
+const moveRight = () => {
+  apps.push(apps.shift());
+  return apps;
+};
+
+const moveLeft = () => {
+  apps.unshift(apps.pop());
+};
